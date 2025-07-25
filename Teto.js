274ef6e44112efba0c605e28625f691123ffc190
Tetto1108 <daniel.chun1108@gmail.com>
2025-07-24 19:50:54 -0600

    Teto.js

--- Teto.js
+++ Teto.js
@@ -1,15 +1,14 @@
 // === Frases posibles para mostrar aleatoriamente ===
 const frases = [
-  "¿Nostalgia acaso?",
-  "¿Antes era mejor no?....",
+  "5MP de pura nostalgia digital",
+  "Cuando el CCD tenía más alma que los megapíxeles",
   "120 MP o simplemente 5MP",
-  "Ser uno más? o tener el vibe",
-  "So can you wonder for a spell and live in parallel",
+  "El auténtico #nofilter era tener una Coolpix",
+  "¿Recuerdas el sonido del obturador digital?",
   "VHS O SD ",
-  "FLash? Porsupuesto",
-  "Personalidad claro",
+  "Fotografía slow-tech en la era hiperdigital",
   "Toj mire esta en mi pagina hola jajaja pongame 100 me desbele haciendo solo la intro ",
-  "Cybershoot, coolpix , easyshare, power shoo y mas.....",
+  "¿Recuerdas a las Cybershoot, Coolpix , Easyshare, Power shot .....?",
 ];
 
 const titleText = document.getElementById("titleText");
